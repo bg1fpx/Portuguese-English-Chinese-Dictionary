8be8ea0b7cdb96bf007790e240be6aa9b00eb857
bg1fpx <zhang_hong@yahoo.com>
2018-12-19 16:37:39 +0800

    Add files via upload

--- search.php
+++ search.php
@@ -14,37 +14,25 @@ if ($length > 20)
 $result = "";
 $total = 0;
 
-// $line = finlandês [fĩlãd'es] (a, m) Finnish, Finn | 芬兰的，芬兰人，芬兰语
-// $po = finlandês
+// $line = finlandes^finlandês [fĩlãd'es] (a, m) Finnish, Finn | 芬兰的，芬兰人，芬兰语
+// $po = finlandes^finlandês
 // $en = Finnish, Finn
 // $ch = 芬兰的，芬兰人，芬兰语
-// $pec = finlandês|Finnish|Finn|芬兰的|芬兰人|芬兰语
+// $poench = finlandes^finlandês|Finnish|Finn|芬兰的|芬兰人|芬兰语
 
 foreach ($dict as $line)
 { $pos1 = stripos($line, " [");
   $pos2 = stripos($line, ") ");
   $pos3 = stripos($line, " | ");
+
   $po = substr($line, 0, $pos1);
   $en = substr($line, $pos2 + 2, $pos3 - $pos2 - 2);
   $ch = substr($line, $pos3 + 3);
-  $poench = $po . "|" . $en . "|" . $ch;
 
-  $poench = strtolower($poench);
-  $poench = str_ireplace("à", "a", $poench);
-  $poench = str_ireplace("á", "a", $poench);
-  $poench = str_ireplace("â", "a", $poench);
-  $poench = str_ireplace("ã", "a", $poench);
-  $poench = str_ireplace("ç", "c", $poench);
-  $poench = str_ireplace("é", "e", $poench);
-  $poench = str_ireplace("ê", "e", $poench);
-  $poench = str_ireplace("í", "i", $poench);
-  $poench = str_ireplace("ó", "o", $poench);
-  $poench = str_ireplace("ô", "o", $poench);
-  $poench = str_ireplace("õ", "o", $poench);
-  $poench = str_ireplace("ú", "u", $poench);
-  $poench = str_ireplace("ü", "u", $poench);
+  $poench = $po . "|" . $en . "|" . $ch;
   $poench = str_ireplace(", ", "|", $poench);
   $poench = str_ireplace("，", "|", $poench);
+  $poench = strtolower($poench);
 
   $list = explode("|", $poench);
   $lastdata = "";
@@ -52,15 +40,21 @@ foreach ($dict as $line)
   if ($mode == "smode")
   { foreach ($list as $word)
       if (strncasecmp($word, $keyword, $length) == 0)
-      { $data = "<li>" . $line;
+      { $pos4 = stripos($line, "^");
+        if ($pos4 > 0)
+          $line = substr($line, $pos4 + 1);
+        $data = "<li>" . $line . "</li>";
         if ($data != $lastdata)
         { $result .= $data;
           $lastdata = $data;
           $total++; } } }
   else
   { foreach ($list as $word)
-      if (stripos($word, $keyword) != false)
-      { $data = "<li>" . $line;
+      if (stripos($word, $keyword) > 0)
+      { $pos4 = stripos($line, "^");
+        if ($pos4 > 0)
+          $line = substr($line, $pos4 + 1);
+        $data = "<li>" . $line . "</li>";
         if ($data != $lastdata)
         { $result .= $data;
           $lastdata = $data;
